a65359d5def9a1490c01486ae14ac002d4724e19
George Len <ghidorah111@gmail.com>
2023-01-30 20:29:23 +0100

    upload project

---
+++ my-react-website/src/components/Analytics.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Raja from '../assets/raja.jpg'
+
+const Analytics = () => {
+    return (
+        <div className='w-full bg-white py-16 px-4'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 gap-5'>
+                <img className='w-[500px] mx-auto my-4 rounded-md' src={Raja} alt="/" />
+                <div className='flex flex-col justify-center'>
+                    <p className='text-[#00E682] font-bold text-xl'>Lorem, ipsum dolor.</p>
+                    <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Lorem ipsum dolor sit amet.</h1>
+                    <p >Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia fugiat, odit obcaecati rem id, iste corrupti perferendis fuga voluptatibus autem vero a voluptas quisquam odio laboriosam repellat, amet reprehenderit quod.</p>
+                    <button className='text-[#00E682] w-[200px] rounded-md font-medium my-6 mx-auto py-3 bg-black md:mx-0 hover:scale-105 duration-300'>Get Started</button>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Analytics
---
+++ my-react-website/src/components/Cards.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Beginner from '../assets/beg.png'
+import Ps from '../assets/psai.png'
+import Js from '../assets/jsnr.png'
+
+const Cards = () => {
+    return (
+        <div className='w-full py-[8rem] px-4 bg-white'>
+            <div className='max-w-[1240px] mx-auto grid md:grid-cols-3 gap-8 '>
+                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-45 mx-auto mt-[-3rem] bg-white' src={Beginner} alt="" />
+                    <h2 className='text-2xl font-bolt text-center py-8'>Beginners Front End</h2>
+                    <p className='text-center text-4xl font-bold'>600 KM</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8 line-through'>800 KM</p>
+                        <p className='py-2 border-b mx-8'>Instruktor: Milorad Smiljanić</p>
+                        <p className='py-2 border-b mx-8'>3 mj treninga + 1 mj pripreme</p>
+                    </div>
+                    <button className='bg-[#00E682] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Sign Up</button>
+                </div>
+
+                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-45 mx-auto mt-[-3rem] bg-white' src={Ps} alt="" />
+                    <h2 className='text-2xl font-bolt text-center py-8'>Graphic Design Basics</h2>
+                    <p className='text-center text-4xl font-bold'>750 KM</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8 line-through'>850 KM</p>
+                        <p className='py-2 border-b mx-8'>Instruktor: Boris Došen</p>
+                        <p className='py-2 border-b mx-8'>2.5 mj treninga + 1 mj pripreme</p>
+                    </div>
+                    <button className='bg-[#00E682] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Sign Up</button>
+                </div>
+
+                <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300'>
+                    <img className='w-45 mx-auto mt-[-3rem] bg-white' src={Js} alt="" />
+                    <h2 className='text-2xl font-bolt text-center py-8'>JavaScript Intermediate</h2>
+                    <p className='text-center text-4xl font-bold'>750 KM</p>
+                    <div className='text-center font-medium'>
+                        <p className='py-2 border-b mx-8 mt-8 line-through'>850 KM</p>
+                        <p className='py-2 border-b mx-8'>Instruktor: Petar Aranđić</p>
+                        <p className='py-2 border-b mx-8'>3 mj treninga + 1 mj pripreme</p>
+                    </div>
+                    <button className='bg-[#00E682] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'>Sign Up</button>
+                </div>
+            </div>
+            <div className='flex'>
+                <button className='text-[#00E682] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 bg-black hover:scale-105 duration-300'>More Courses ➜</button>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
